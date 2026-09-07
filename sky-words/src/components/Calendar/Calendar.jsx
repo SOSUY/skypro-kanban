8c78db810572f45a331ecc53bec734c626aeea46
SLAVEik <sanek21rusg@gmail.com>
2026-09-07 12:25:58 +0300

    Delete sky-words directory

--- sky-words/src/components/Calendar/Calendar.jsx
+++
@@ -1,9 +0,0 @@
-import "../App/App.css";
-
-export default function Calendar() {
-    return (
-        <div className="calendar">
-            <p>📅 Здесь будет календарь</p>
-        </div>
-    );
-}
